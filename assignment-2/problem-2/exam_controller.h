15aea1bc53077caf9e73eae54c494e2598dd7723
risenfromashes <ashrafurkhan37@gmail.com>
2023-01-04 05:48:54 +0600

    start assignment-2

--- assignment-2/problem-2/exam_controller.h
+++ assignment-2/problem-2/exam_controller.h
@@ -7,7 +7,7 @@ class ExamController : public ExamMediator{
   public:
     void notify(std::shared_ptr<Student> sender, ReexamineRequestEvent event) override;
     void notify(std::shared_ptr<Teacher> sender, ReexamineResultEvent event) override;
-    void notify(std::shared_ptr<Teacher> sender, ReexamineRequestEvent event) override;
+    void notify(std::shared_ptr<Teacher> sender, ResultEvent event) override;
     void register_student(std::shared_ptr<Student> student) override;
     void register_teacher(std::shared_ptr<Teacher> teacher) override;
 };
--- assignment-2/problem-2/exam_mediator.h
+++ assignment-2/problem-2/exam_mediator.h
@@ -10,9 +10,8 @@ class Teacher;
 class ExamMediator {
   public:
     virtual void notify(std::shared_ptr<Student> sender, ReexamineRequestEvent event) = 0;
-
     virtual void notify(std::shared_ptr<Teacher> sender, ReexamineResultEvent event) = 0;
-    virtual void notify(std::shared_ptr<Teacher> sender, ReexamineRequestEvent event) = 0;
+    virtual void notify(std::shared_ptr<Teacher> sender, ResultEvent event) = 0;
 
     virtual void register_student(std::shared_ptr<Student> student) = 0;
     virtual void register_teacher(std::shared_ptr<Teacher> teacher) = 0;
