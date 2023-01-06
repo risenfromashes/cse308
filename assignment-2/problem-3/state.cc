77b9982200035fac090929748238697fffc3673b
risenfromashes <ashrafurkhan37@gmail.com>
2023-01-06 06:04:08 +0600

    work on problem-3

--- assignment-2/problem-3/state.cc
+++ assignment-2/problem-3/state.cc
@@ -1,18 +1,43 @@
 #include "state.h"
 #include <fmt/core.h>
 
-void State::add_money() { //
-  fmt::print("Cannot add money in this state\n");
+void VendingMachineState::add_money() { //
+  fmt::print("[Response] Cannot add money at the moment\n");
 }
 
-void State::get_return() { //
-  fmt::print("Cannot add money in this state\n");
+void VendingMachineState::get_return() { //
+  fmt::print("[Response] Cannot add money at the moment\n");
 }
 
-void State::get_product() { //
-  fmt::print("Cannot get product in this state\n");
+void VendingMachineState::refill_product() { //
+  fmt::print("[Response] Refill not required currently\n");
 }
 
-void State::refill_product() { //
-  fmt::print("Cannot refill product in this state\n");
+void VendingMachineState::get_product() { //
+  fmt::print("[Response] Cannot get product at the moment\n");
 }
+
+void StartState::add_money() {}
+void StartState::get_return() {}
+void StartState::get_product() {}
+void StartState::refill_product() {}
+
+void UnpaidState::add_money() {}
+void UnpaidState::get_return() {}
+void UnpaidState::get_product() {}
+void UnpaidState::refill_product() {}
+
+void PaidState::add_money() {}
+void PaidState::get_return() {}
+void PaidState::get_product() {}
+void PaidState::refill_product() {}
+
+void OverpaidState::add_money() {}
+void OverpaidState::get_return() {}
+void OverpaidState::get_product() {}
+void OverpaidState::refill_product() {}
+
+void VacantState::add_money() {}
+void VacantState::get_return() {}
+void VacantState::get_product() {}
+void VacantState::refill_product() {}
--- assignment-2/problem-3/state.h
+++ assignment-2/problem-3/state.h
@@ -3,12 +3,6 @@
 #include "vending_machine.h"
 
 class State {
-public:
-  virtual void add_money();
-  virtual void get_return();
-  virtual void refill_product();
-  virtual void get_product();
-
 protected:
   State(Context *context);
 
@@ -16,9 +10,61 @@ protected:
 };
 
 class VendingMachineState : public State {
+public:
+  /* default implementations */
+
+  // don't accept money
+  virtual void add_money();
+  // don't provide return
+  virtual void get_return();
+  // don't return product
+  virtual void get_product();
+  // don't accept refill
+  virtual void refill_product();
+
 protected:
   VendingMachine *context();
+  virtual ~VendingMachineState() = default;
+
   VendingMachineState(VendingMachine *vm);
 };
 
-class StartState : public VendingMachineState {};
+class StartState : public VendingMachineState {
+public:
+  void add_money() override;
+  void get_return() override;
+  void get_product() override;
+  void refill_product() override;
+};
+
+class UnpaidState : public VendingMachineState {
+public:
+  void add_money() override;
+  void get_return() override;
+  void get_product() override;
+  void refill_product() override;
+};
+
+class PaidState : public VendingMachineState {
+public:
+  void add_money() override;
+  void get_return() override;
+  void get_product() override;
+  void refill_product() override;
+};
+
+class OverpaidState : public VendingMachineState {
+public:
+  void add_money() override;
+  void get_return() override;
+  void get_product() override;
+  void refill_product() override;
+};
+
+class VacantState : public VendingMachineState {
+public:
+  void add_money() override;
+  void get_return() override;
+  void get_product() override;
+  void refill_product() override;
+};
